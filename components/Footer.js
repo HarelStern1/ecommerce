75de232a91bcf7b2b2da927eeb666d8d3b7a206a
Harel Stern <harel@Harels-MacBook-Pro.local>
2022-07-23 03:43:21 +0300

    initial commit

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <a href="https://github.com/HarelStern1" rel="nofolloe" target="_blank">
+      <a href="https://github.com/HarelStern1">
         Created by Harel Stern With ReactJS & NextJS
       </a>
     </div>
